bb73bb41f8451635a5d38e80aafc3323c872aad2
Ryan Friedman <rmfried3@ncsu.edu>
2026-07-09 00:06:09 -0700

    Radar chart plot completed

--- helpers.R
+++ helpers.R
@@ -1,13 +1,11 @@
+# variable labels for use in tables and plots
 var_labels <- c(
-  # Categorical / identifier
   player             = "Player",
   tour               = "Tour",
   surface            = "Court Surface",
   round              = "Tournament Round",
   handed             = "Handedness",
   year               = "Year",
-  
-  # Raw count stats
   serve_pts          = "Serve Points",
   aces               = "Aces",
   dfs                = "Double Faults",
@@ -26,8 +24,6 @@ var_labels <- c(
   unforced_fh        = "Forehand Unforced Errors",
   unforced_bh        = "Backhand Unforced Errors",
   total_pts          = "Total Points",
-  
-  # Derived percentage stats
   first_in_pct       = "First Serve In %",
   ace_pct            = "Ace %",
   first_won_pct      = "First Serve Points Won %",
@@ -42,5 +38,31 @@ var_labels <- c(
   winner_fh_pct      = "Forehand Winner %",
   winner_bh_pct      = "Backhand Winner %",
   unforced_fh_pct    = "Forehand Unforced Error %",
-  unforced_bh_pct    = "Backhand Unforced Error %"
+  unforced_bh_pct    = "Backhand Unforced Error %",
+  fh_eff_pct         = "Forehand Effectiveness %",
+  bh_eff_pct         = "Backhand Effectiveness %",
+  aggression_pct     = "Aggression %"
 )
+
+# truncated labels just for the radar chart 
+radar_labels <- c(
+  total_won_pct      = "Total Won %",
+  serve_pts_won_pct  = "Serve Won %",
+  return_pts_won_pct = "Return Won %",
+  ace_pct            = "Ace %",
+  aggression_pct     = "Aggression %",
+  fh_eff_pct         = "FH Effect. %",
+  bh_eff_pct         = "BH Effect. %"
+)
+
+# defining min/max to scale each variable of the radar chart
+radar_bounds <- data.frame(
+  total_won_pct      = c(0.60, 0.40),
+  ace_pct            = c(0.25, 0.00),
+  serve_pts_won_pct  = c(0.75, 0.45),
+  return_pts_won_pct = c(0.55, 0.20),
+  aggression_pct     = c(0.50, 0.20),
+  fh_eff_pct         = c(0.05, -0.08),
+  bh_eff_pct         = c(0.03, -0.10)
+)
+rownames(radar_bounds) <- c("max", "min")
